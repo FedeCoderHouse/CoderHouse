36a527172ab006b9a4977bea5cd8d9f0b09adc9e
Federico Matias Bianco <fbianco@technisys.com>
2022-03-13 15:02:18 -0300

    primera subida de archivos

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+
+
+
+function NavBar(){
+    return (
+
+    <header>
+    <img src="logo512.png" className="App-logo" alt="logo" />
+    <div>
+        <ul className="navbar">
+            <li><button>Home</button></li>
+            <li><button>Productos</button></li>
+            <li><button>Nosotros</button></li>
+            <li><button>Contacto</button></li>
+        </ul>
+    </div>
+    </header>
+    );
+}
+
+export default NavBar;
