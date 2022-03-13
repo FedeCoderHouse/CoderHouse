67216aa728c52403cbae265e6e3f7dad29900f35
FedeCoderHouse <fedeb.fede@gmail.com>
2022-03-13 17:12:38 -0300

    IdeaFedericoBianco

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
-
-
+import Button from '@mui/material/Button';
 
 function NavBar(){
     return (
 
     <header>
     <img src="logo512.png" className="App-logo" alt="logo" />
+    <h1>TiendaUno</h1>
     <div>
         <ul className="navbar">
-            <li><button>HomeHome</button></li>
-            <li><button>Productos</button></li>
-            <li><button>Nosotros</button></li>
-            <li><button>Contacto</button></li>
+            <li><Button variant="outlined">Home</Button></li>
+            <li><Button variant="outlined">Productos</Button></li>
+            <li><Button variant="outlined">Nosotros</Button></li>
+            <li><Button variant="outlined">Contacto</Button></li>
         </ul>
     </div>
     </header>
